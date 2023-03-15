ebab2cb875dfbc63c29fe47b48bb667c44d66a35
Stargazer7 <1601778118@qq.com>
2023-03-15 23:29:57 +0800

    hide

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.headers['Authorization'] = 'Bearer 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'
-axios.defaults.headers['organization '] = 'org-0O4E9R9cJRucPALGtEQZ5Ygz'
+axios.defaults.headers['Authorization'] = 'Bearer '+''
+axios.defaults.headers['organization '] = ''
