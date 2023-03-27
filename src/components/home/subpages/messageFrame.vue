c8aa6da3ed5af0cb4ad666248c37b7d21d25616d
Stargazer7 <1601778118@qq.com>
2023-03-27 16:54:21 +0800

    scroll to bottom when new message

--- src/components/home/subpages/messageFrame.vue
+++ src/components/home/subpages/messageFrame.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="frame">
-    <el-scrollbar>
-      <div class="chat-content">
+    <el-scrollbar ref="scrollRef">
+      <div class="chat-content" ref="chatContentRef">
         <SingleMessage
           v-for="(item, index) in chatMessages"
           :chat-message="item"
@@ -19,12 +19,29 @@
 import InputBox from './messageFrame/inputBox.vue';
 import SingleMessage from './messageFrame/singleMessage.vue';
 import type { ChatMessage } from '@/stores/modules/chat';
+import { computed, ref, watch } from 'vue';
+import { ElScrollbar } from 'element-plus';
 
 type Props = {
   chatMessages: ChatMessage[];
-  showStatus:boolean
+  showStatus: boolean;
 };
 const props = defineProps<Props>();
+
+const scrollRef = ref<InstanceType<typeof ElScrollbar>>();
+const chatContentRef = ref<HTMLDivElement>()
+const messageAmount = computed(() => {
+  return props.chatMessages.length;
+});
+
+watch(messageAmount, (newVal, oldVal) => {
+  if (newVal > oldVal) {
+    // console.log(scrollRef.value?.$el.offsetHeight,'scroll');
+    // console.log(chatContentRef.value?.offsetHeight,'div');
+    
+    scrollRef.value?.scrollTo(0, chatContentRef.value?.offsetHeight);
+  }
+});
 
 const emits = defineEmits(['on-userInput']);
 
@@ -41,6 +58,7 @@ const handleUserInput = (text: string) => {
   border: 1px solid #e4e4e4;
   .chat-content {
     flex: 1;
+    padding-bottom: 50%;
   }
 }
 </style>
